9ed5dcdb16822be9fa2462c86b6939779d7d09a5
nsoevik <njordsoevik@gmail.com>
2022-03-27 19:54:38 -0400

    dislikes schema and model

--- mongoose/dislikes/DislikeModel.ts
+++ mongoose/dislikes/DislikeModel.ts
@@ -4,5 +4,5 @@
  */
 import mongoose from "mongoose";
 import DislikeSchema from "./DislikeSchema";
-const DislikeModel = mongoose.model("LikeModel", DislikeSchema);
+const DislikeModel = mongoose.model("DislikeModel", DislikeSchema);
 export default DislikeModel;
