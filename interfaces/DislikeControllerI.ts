7c4346a7274090f940c80bd7c0dea4988dfdc937
nsoevik <njordsoevik@gmail.com>
2022-03-28 13:16:16 -0400

    add controller and dao

--- interfaces/DislikeControllerI.ts
+++ interfaces/DislikeControllerI.ts
@@ -2,4 +2,6 @@ import {Request, Response} from "express";
 
 export default interface DislikeControllerI {
     userTogglesTuitDislikes (req: Request, res: Response): void;
+    findAllUsersThatDislikedTuit (req: Request, res: Response): void;
+    findAllTuitsDislikedByUser (req: Request, res: Response): void;
 };
--- interfaces/DislikeDaoI.ts
+++ interfaces/DislikeDaoI.ts
@@ -1,9 +1,11 @@
-import Like from "../models/dislikes/Dislike";
+import Dislike from "../models/dislikes/Dislike";
 
 /**
  * @file Declares API for Dislikes related data access object methods
  */
 export default interface DislikeDaoI {
+    findAllUsersThatDislikedTuit (tid: string): Promise<Dislike[]>;
+    findAllTuitsDislikedByUser (uid: string): Promise<Dislike[]>;
     userUndislikesTuit (tid: string, uid: string): Promise<any>;
-    userDislikesTuit (tid: string, uid: string): Promise<Like>;
+    userDislikesTuit (tid: string, uid: string): Promise<Dislike>;
 };
